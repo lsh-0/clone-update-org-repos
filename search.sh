0c669cb4a8a5dd08efc11381c6d34c61cc6e6682
Luke <lsh-0@users.noreply.github.com>
2022-04-04 14:27:34 +0930

    search.sh, further args can be provided to ripgrep by appending them.

--- search.sh
+++ search.sh
@@ -2,13 +2,18 @@
 # searches the cloned repos for mentions of given search term
 set -ex
 val="$1"
+shift
+clear
 rg "$val" \
     --no-ignore --no-hidden --no-binary \
     --glob-case-insensitive \
     --max-filesize 5M \
+    -g '!elifesciences--repos--page*.json' \
     -g '!*.ipynb' \
     -g '!*.xml' \
+    -g '!*.svg' \
     -g '!*.header' \
     -g '!*.checkin' \
     -g '!*.new' \
-    -g '!*.generated*'
+    -g '!*.generated*' \
+    "$@"
